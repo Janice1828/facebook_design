9e82a6e39ee12bd1814d2189b97560369a6b85a1
Jenish Limbu <jlimbu539@gmail.com>
2023-06-26 20:37:27 +0545

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,56 @@
 import Form from "react-bootstrap/Form";
-
+import { Row, Col } from "react-bootstrap";
+import { FaFacebook, FaFontAwesome } from "react-icons/fa";
+import { AiFillHome } from "react-icons/ai";
+import { GiBlockHouse } from "react-icons/gi";
+import { HiUserGroup } from "react-icons/hi";
+import { TbGridDots } from "react-icons/tb";
+import { CgGames } from "react-icons/cg";
+import { IoMdNotifications } from "react-icons/io";
+import { BsMessenger } from "react-icons/bs";
+import { BiSolidUser } from "react-icons/bi";
+import { IconContext } from "react-icons";
+import { BsSearch } from "react-icons/bs";
 function TextControlsExample() {
+  const placeHolderIcons = <BsSearch />;
   return (
     <>
-      <Form>
-        <Form.Control type="text" placeholder="search"></Form.Control>
-      </Form>
+      <Row className="px-4 py-2 shadow align-items-center mb-3">
+        <Col className="d-flex gap-2">
+          <FaFacebook size="40px" color="#3b5998"></FaFacebook>
+          <Form>
+            <Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Search Facebook"
+                style={{ borderRadius: "25px" }}
+              ></Form.Control>
+            </Form.Label>
+          </Form>
+        </Col>
+        <Col className="d-flex justify-content-around">
+          <IconContext.Provider value={{ color: "#65676B", size: "25px" }}>
+            <AiFillHome></AiFillHome>
+            <GiBlockHouse></GiBlockHouse>
+            <HiUserGroup></HiUserGroup>
+            <CgGames></CgGames>
+          </IconContext.Provider>
+        </Col>
+        <Col className="d-flex gap-3 justify-content-end">
+          <IconContext.Provider
+            value={{
+              color: "#000",
+              size: "25px",
+              className: "bg-secondary bg-opacity-50",
+            }}
+          >
+            <TbGridDots className="p-1 rounded-4"></TbGridDots>
+            <BsMessenger className="p-1 rounded-4"></BsMessenger>
+            <IoMdNotifications className="p-1 rounded-4"></IoMdNotifications>
+            <BiSolidUser className="p-1 rounded-4"></BiSolidUser>
+          </IconContext.Provider>
+        </Col>
+      </Row>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,21 +3,24 @@ import Navbar from "../components/Navbar";
 import Shortcuts from "../components/Shortcuts";
 import Newsfeed from "../components/Newsfeed";
 import Contacts from "../components/Contacts";
+import { Container } from "react-bootstrap";
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <div className="row">
-        <div className="col-4">
-          <Shortcuts />
+      <Container fluid>
+        <Navbar />
+        <div className="row">
+          <div className="col-4">
+            <Shortcuts />
+          </div>
+          <div className="col-4">
+            <Newsfeed />
+          </div>
+          <div className="col-4">
+            <Contacts />
+          </div>
         </div>
-        <div className="col-4">
-          <Newsfeed />
-        </div>
-        <div className="col-4">
-          <Contacts />
-        </div>
-      </div>
+      </Container>
     </>
   );
 };
