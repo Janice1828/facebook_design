765672c2875ae450bb7abf9ea476389da077f009
Jenish Limbu <jlimbu539@gmail.com>
2023-06-29 21:56:19 +0545

    marketplace content added

---
+++ src/marketplace/MarketData.js
@@ -0,0 +1,76 @@
+import dress from "../img/team-3.jpg";
+const Marketdata = [
+  {
+    image: dress,
+    cost: "12000",
+    title: "One piece",
+    location: "Kathmandu Nepal",
+  },
+  {
+    image: dress,
+    cost: "12000",
+    title: "Jeans Pant",
+    location: "Kathmandu Nepal",
+  },
+  {
+    image: dress,
+    cost: "12000",
+    title: "Hoodie",
+    location: "Kathmandu Nepal",
+  },
+  {
+    image: dress,
+    cost: "12000",
+    title: "Cycle",
+    location: "Kathmandu Nepal",
+  },
+  {
+    image: dress,
+    cost: "12000",
+    title: "Bike",
+    location: "Kathmandu Nepal",
+  },
+  {
+    image: dress,
+    cost: "12000",
+    title: "One piece",
+    location: "Kathmandu Nepal",
+  },
+  {
+    image: dress,
+    cost: "12000",
+    title: "One piece",
+    location: "Kathmandu Nepal",
+  },
+  {
+    image: dress,
+    cost: "12000",
+    title: "One piece",
+    location: "Kathmandu Nepal",
+  },
+  {
+    image: dress,
+    cost: "12000",
+    title: "One piece",
+    location: "Kathmandu Nepal",
+  },
+  {
+    image: dress,
+    cost: "12000",
+    title: "One piece",
+    location: "Kathmandu Nepal",
+  },
+  {
+    image: dress,
+    cost: "12000",
+    title: "One piece",
+    location: "Kathmandu Nepal",
+  },
+  {
+    image: dress,
+    cost: "12000",
+    title: "One piece",
+    location: "Kathmandu Nepal",
+  },
+];
+export default Marketdata;
--- src/marketplace/Marketcontent.js
+++ src/marketplace/Marketcontent.js
@@ -1,25 +1,44 @@
 import React from "react";
 import goods from "../img/blog-1.jpg";
-const Marketcontent = () => {
+import Marketdata from "./MarketData";
+const MarketCard = (props) => {
   return (
     <>
       <div>
-        <div className="d-flex justify-content-between">
-          <h6>Today's Picks</h6>
-          <p>Kathmandu . 65 km</p>
-        </div>
         <div className="d-flex flex-column">
           <img
-            src={goods}
+            src={props.image}
             alt="goods"
             width="200px"
             height="230px"
             style={{ borderRadius: "20px" }}
           />
-          <strong>FREE</strong>
-          <p>Summer Outfit for boys</p>
-          <small>Kathmandu, Bagmati</small>
+          <div className="">
+            <strong>{props.price}</strong>
+            <p>{props.title}</p>
+            <small>{props.location}</small>
+          </div>
         </div>
+      </div>
+    </>
+  );
+};
+const Marketcontent = () => {
+  return (
+    <>
+      <div className="d-flex justify-content-between">
+        <h6>Today's Picks</h6>
+        <p>Kathmandu . 65 km</p>
+      </div>
+      <div style={{ display: "grid", gridTemplateColumns: "25% 25% 25% 25%" }}>
+        {Marketdata.map((val) => (
+          <MarketCard
+            title={val.title}
+            price={val.cost}
+            location={val.location}
+            image={val.image}
+          />
+        ))}
       </div>
     </>
   );
