a662a31332f6892e49dc01ce72d9bb0ef36e662c
Jenish Limbu <jlimbu539@gmail.com>
2023-07-01 11:31:27 +0545

    group data

---
+++ src/Groups/Groupdata.js
@@ -0,0 +1,104 @@
+import groupImg from "../img/team-1.jpg";
+const Groupdata = [
+  {
+    groupName: "Intern Sathi",
+    userName: "Jenish Limbu",
+    content:
+      "Join our winning team! Codavatar is hiring for various positions and looking for talented individuals to be part of our success story. Apply now and take the first step towards an exciting and rewarding career with us.Don miss out on this incredible opportunity! Apply at vacancy@codavatar.com. *Remember to define the subject line to the job for which you are applying.*#Hiring #team #careerOpportunity",
+    image: groupImg,
+    like: "Samir Shrestha and 3k others",
+    comment: "20 comments",
+    shares: "20 shares",
+  },
+  {
+    groupName: "Intern Nepal",
+    userName: "Jenish Limbu",
+    content:
+      "Join our winning team! Codavatar is hiring for various positions and looking for talented individuals to be part of our success story. Apply now and take the first step towards an exciting and rewarding career with us.Don miss out on this incredible opportunity! Apply at vacancy@codavatar.com. *Remember to define the subject line to the job for which you are applying.*#Hiring #team #careerOpportunity",
+    image: groupImg,
+    like: "Samir Shrestha and 3k others",
+    comment: "20 comments",
+    shares: "20 shares",
+  },
+  {
+    groupName: "Routine Of Nepal Banda",
+    userName: "Jenish Limbu",
+    content:
+      "Join our winning team! Codavatar is hiring for various positions and looking for talented individuals to be part of our success story. Apply now and take the first step towards an exciting and rewarding career with us.Don miss out on this incredible opportunity! Apply at vacancy@codavatar.com. *Remember to define the subject line to the job for which you are applying.*#Hiring #team #careerOpportunity",
+    image: groupImg,
+    like: "Samir Shrestha and 3k others",
+    comment: "20 comments",
+    shares: "20 shares",
+  },
+  {
+    groupName: "Padmashree Confession",
+    userName: "Jenish Limbu",
+    content:
+      "Join our winning team! Codavatar is hiring for various positions and looking for talented individuals to be part of our success story. Apply now and take the first step towards an exciting and rewarding career with us.Don miss out on this incredible opportunity! Apply at vacancy@codavatar.com. *Remember to define the subject line to the job for which you are applying.*#Hiring #team #careerOpportunity",
+    image: groupImg,
+    like: "Samir Shrestha and 3k others",
+    comment: "20 comments",
+    shares: "20 shares",
+  },
+  {
+    groupName: "Milestone Confession",
+    userName: "Jenish Limbu",
+    content:
+      "Join our winning team! Codavatar is hiring for various positions and looking for talented individuals to be part of our success story. Apply now and take the first step towards an exciting and rewarding career with us.Don miss out on this incredible opportunity! Apply at vacancy@codavatar.com. *Remember to define the subject line to the job for which you are applying.*#Hiring #team #careerOpportunity",
+    image: groupImg,
+    like: "Samir Shrestha and 3k others",
+    comment: "20 comments",
+    shares: "20 shares",
+  },
+  {
+    groupName: "Intern Sathi",
+    userName: "Jenish Limbu",
+    content:
+      "Join our winning team! Codavatar is hiring for various positions and looking for talented individuals to be part of our success story. Apply now and take the first step towards an exciting and rewarding career with us.Don miss out on this incredible opportunity! Apply at vacancy@codavatar.com. *Remember to define the subject line to the job for which you are applying.*#Hiring #team #careerOpportunity",
+    image: groupImg,
+    like: "Samir Shrestha and 3k others",
+    comment: "20 comments",
+    shares: "20 shares",
+  },
+  {
+    groupName: "Intern Sathi",
+    userName: "Jenish Limbu",
+    content:
+      "Join our winning team! Codavatar is hiring for various positions and looking for talented individuals to be part of our success story. Apply now and take the first step towards an exciting and rewarding career with us.Don miss out on this incredible opportunity! Apply at vacancy@codavatar.com. *Remember to define the subject line to the job for which you are applying.*#Hiring #team #careerOpportunity",
+    image: groupImg,
+    like: "Samir Shrestha and 3k others",
+    comment: "20 comments",
+    shares: "20 shares",
+  },
+  {
+    groupName: "Intern Sathi",
+    userName: "Jenish Limbu",
+    content:
+      "Join our winning team! Codavatar is hiring for various positions and looking for talented individuals to be part of our success story. Apply now and take the first step towards an exciting and rewarding career with us.Don miss out on this incredible opportunity! Apply at vacancy@codavatar.com. *Remember to define the subject line to the job for which you are applying.*#Hiring #team #careerOpportunity",
+    image: groupImg,
+    like: "Samir Shrestha and 3k others",
+    comment: "20 comments",
+    shares: "20 shares",
+  },
+  {
+    groupName: "Intern Sathi",
+    userName: "Jenish Limbu",
+    content:
+      "Join our winning team! Codavatar is hiring for various positions and looking for talented individuals to be part of our success story. Apply now and take the first step towards an exciting and rewarding career with us.Don miss out on this incredible opportunity! Apply at vacancy@codavatar.com. *Remember to define the subject line to the job for which you are applying.*#Hiring #team #careerOpportunity",
+    image: groupImg,
+    like: "Samir Shrestha and 3k others",
+    comment: "20 comments",
+    shares: "20 shares",
+  },
+  {
+    groupName: "Intern Sathi",
+    userName: "Jenish Limbu",
+    content:
+      "Join our winning team! Codavatar is hiring for various positions and looking for talented individuals to be part of our success story. Apply now and take the first step towards an exciting and rewarding career with us.Don miss out on this incredible opportunity! Apply at vacancy@codavatar.com. *Remember to define the subject line to the job for which you are applying.*#Hiring #team #careerOpportunity",
+    image: groupImg,
+    like: "Samir Shrestha and 3k others",
+    comment: "20 comments",
+    shares: "20 shares",
+  },
+];
+export default Groupdata;
--- src/pageContent/Groupcontent.js
+++ src/pageContent/Groupcontent.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Row, Col, Card, Button } from "react-bootstrap";
 import groupImg from "../img/feature.jpg";
-const Groupcontent = () => {
+import Groupdata from "../Groups/Groupdata";
+const Groupcard = (props) => {
   return (
     <>
       <div>
@@ -9,29 +10,22 @@ const Groupcontent = () => {
           <Col>
             <Card>
               <Card.Header>
-                <p>IT JOBS AND INTERNS</p>
-                <small>codavatar</small>
+                <p>{props.groupName}</p>
+                <small>{props.userName}</small>
               </Card.Header>
               <Card.Body>
-                <Card.Text>
-                  Join our winning team! 🌟
-                  <br /> Codavatar is hiring for various positions and looking
-                  for talented individuals to be part of our success story.
-                  Apply now and take the first step towards an exciting and
-                  rewarding career with us.
-                  <br /> Don't miss out on this incredible opportunity! <br />
-                  Apply at vacancy@codavatar.com. *Remember to define the
-                  subject line to the job for which you are applying.*
-                  <span variant="primary">
-                    #Hiring #team #careerOpportunity
-                  </span>
-                </Card.Text>
-                <Card.Img src={groupImg}></Card.Img>
+                <Card.Text>{props.content}</Card.Text>
+                <Card.Img src={props.image}></Card.Img>
                 <Card.Footer>
+                  <div className="d-flex justify-content-between">
+                    <span>{props.like}</span>
+                    <span>{props.comment}</span>
+                    <span>{props.share}</span>
+                  </div>
                   <div className="d-flex justify-content-around">
-                    <Button>Like</Button>
-                    <Button>Comment</Button>
-                    <Button>Share</Button>
+                    <Button id="reButtons">Like</Button>
+                    <Button id="reButtons">Comment</Button>
+                    <Button id="reButtons">Share</Button>
                   </div>
                 </Card.Footer>
               </Card.Body>
@@ -39,6 +33,23 @@ const Groupcontent = () => {
           </Col>
         </Row>
       </div>
+    </>
+  );
+};
+const Groupcontent = () => {
+  return (
+    <>
+      {Groupdata.map((val) => (
+        <Groupcard
+          groupName={val.groupName}
+          userName={val.userName}
+          image={val.image}
+          content={val.content}
+          like={val.like}
+          comment={val.comment}
+          share={val.shares}
+        />
+      ))}
     </>
   );
 };
