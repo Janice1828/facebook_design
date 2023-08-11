e85337bb03d98a25ad445b24675eb63ed2c53b38
Jenish Limbu <jlimbu539@gmail.com>
2023-08-11 21:46:18 +0545

    contacts updated

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,75 +1,96 @@
 import React from "react";
 import { BiSolidUser } from "react-icons/bi";
+import one from "../img/people/one.jpg";
+import two from "../img/people/two.jpg"
+import three from "../img/people/three.jpg"
+import four from "../img/people/four.jpg"
+import five from "../img/people/five.jpg"
+import six from "../img/people/six.jpg"
+import seven from "../img/people/seven.jpg"
+import eight from "../img/people/eight.jpg"
+import nine from "../img/people/people10.jpg";
+import ten from "../img/people/team-1.jpg";
+
+import eleven from "../img/people/team-2.jpg";
+import twelve from "../img/people/team-3.jpg";
+import thirteen from "../img/people/team-4.jpg";
+import fourteen from "../img/people/testimonial-1.jpg";
+import fifteen from "../img/people/testimonial-2.jpg";
+import sixteen from "../img/people/testimonial-4.jpg";
+import seventeen from "../img/people/two.jpg";
+import eighteen from "../img/people/four.jpg";
+import nineteen from "../img/people/five.jpg";
 const Contacts = () => {
   return (
     <div className="d-flex flex-column gap-3 shortcut">
       <h6>Contacts</h6>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={one} alt="Image"/>
         <h6>Dilip Limbu</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={two} alt="Image"/>
         <h6>Sachin Paudel</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={three} alt="Image"/>
+     
         <h6>Santa Limbu</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={four} alt="Image"/>
         <h6>Dipa Chamling</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={five} alt="Image"/>
         <h6>Bhima Imsong</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={six} alt="Image"/>
         <h6>Jenish Thapa Magar</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={seven} alt="Image"/>
         <h6>Arun Tamang</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={eight} alt="Image"/>
         <h6>Madan Rai</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={eleven} alt="Image"/>
         <h6>Dipika Chaudhary</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={twelve} alt="Image"/>
         <h6>Ram Chemjong</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={thirteen} alt="Image"/>
         <h6>Manoj Rai</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={fourteen} alt="Image"/>
         <h6>Solti Chemjong</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={fifteen} alt="Image"/>
         <h6>Sunita Sunita</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={sixteen} alt="Image"/>
         <h6>Biswash Shrestha</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={seventeen} alt="Image"/>
         <h6>Karan Limbu</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={eighteen} alt="Image"/>
         <h6>Pramod Pramod</h6>
       </div>
       <div className="d-flex gap-2 align-items-center">
-        <BiSolidUser size="25px"></BiSolidUser>
+      <img class="shortcutsImgs" src={nineteen} alt="Image"/>
         <h6>Bhesh Raj Timilshana</h6>
       </div>
     </div>
